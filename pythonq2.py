67677f1a4cd6ae386798b4accfafa267bab9e763
sandeepuni <sandeep00uni@gmail.com>
2025-11-19 20:03:07 +0530

    Update pythonq2.py

--- pythonq2.py
+++ pythonq2.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+"""
+2.	An App for Mobile hosting provider charges- $0.51 per hour how much does it cost to operator per day, per week, per month? how many days can i operate one server with$918?
+"""
 
 costph = 0.51
 # Cost per Day

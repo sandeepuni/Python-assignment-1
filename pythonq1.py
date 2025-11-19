8aa6712e33c9a17b0a55d2fb3d4893e45f1a7f20
sandeepuni <sandeep00uni@gmail.com>
2025-11-19 20:03:31 +0530

    Update pythonq1.py

--- pythonq1.py
+++ pythonq1.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+"""
+1.	Create a python program that asks the user how far they want to travel. If they want to travel less than three miles tell them to ride Bicycle. if they want to travel more than three miles, but less than three hundred miles, tell them to ride Motor-Cycle. if they want to travel three hundred miles or more tell them to drive Super-Car
+"""
 
 distance = int(input("How far do you want to travel (in miles)? "))
 

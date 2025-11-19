c04736d7998757382f4804ad1370505c4fb879d4
sandeepuni <sandeep00uni@gmail.com>
2025-11-19 20:01:57 +0530

    Update pythonq4.py

--- pythonq4.py
+++ pythonq4.py
@@ -1,4 +1,9 @@
+"""
+4.	A traffic system records the average speed of vehicles every hour for 12 hours.
+Write a program that: Reads 12 speed values (floats). Calculates the average speed. Displays whether traffic flow was “Slow” (avg < 40), “Normal” (40–80), or “Fast” (avg > 80).
+Concepts: list input, loop, conditional check, average calculation.
 
+"""
 speed = []
 print("Please enter the 12 speeds")
 for i in range(12):
@@ -11,4 +16,4 @@ elif 40 <= Average <=80 :
     print("Normal")
 else :
     print("Fast")
-44
+

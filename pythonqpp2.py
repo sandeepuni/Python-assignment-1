22c588c988a8538993a21c2c8725147f0210c6f6
sandeepuni <sandeep00uni@gmail.com>
2025-11-21 08:25:00 +0530

    Update pythonqpp2.py

--- pythonqpp2.py
+++ pythonqpp2.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+"""
+5.	Design a module math_utils.py with functions:
+factorial(n)
+is_prime(n)
+gcd(a,b)
+Include exception handling for invalid or negative inputs. Import and test these functions from another script.
+Concepts: Loops, recursion, functions, input validation.
+
+"""
 
 import math_utils
 n = input("Please insert a number to calculate factorial and check if its prime")
